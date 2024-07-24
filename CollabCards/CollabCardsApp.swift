20af0a861c8b7d1d3babf4cf148d5e7f03da1302
furkanfatihkok <furkankok296@gmail.com>
2024-07-24 15:05:02 +0300

    first commit

---
+++ CollabCards/CollabCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CollabCardsApp.swift
+//  CollabCards
+//
+//  Created by FFK on 24.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct CollabCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
