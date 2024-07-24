ef93962ad508948ce09cc18c51e7309e726903a3
furkanfatihkok <furkankok296@gmail.com>
2024-07-24 15:16:54 +0300

    add View

---
+++ CollabCards/RetroView.swift
@@ -0,0 +1,18 @@
+//
+//  RetroView.swift
+//  CollabCards
+//
+//  Created by FFK on 24.07.2024.
+//
+
+import SwiftUI
+
+struct RetroView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RetroView()
+}
