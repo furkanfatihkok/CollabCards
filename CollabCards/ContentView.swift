71781894ccb7596d1ae0eb0c3d6a9f3fea656e1c
furkanfatihkok <furkankok296@gmail.com>
2024-07-26 11:22:06 +0300

    home klasörümün altında oluşturduğum view klasörümde ki homeView'ın adını EmptyView olarak güncelledim .RetroView için duratiınSettingsView ve WelcomeSelectionView componentinlerini  oluşturdum.

--- CollabCards/ContentView.swift
+++ CollabCards/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         TabView {
-            HomeView()
+            EmptyView()
                 .tabItem {
                     Image(systemName: "house.fill")
                     Text("Home")
