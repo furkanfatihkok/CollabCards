8d324856cfcb8bc26bcbc49f395d4740d18f3ea5
furkanfatihkok <furkankok296@gmail.com>
2024-08-15 12:49:16 +0300

    - BoardViewModel ve CardViewModel için temel unit testler eklendi. Testler, boardların ve cardların doğru şekilde çekildiğini, eklendiğini, güncellendiğini ve silindiğini kontrol ediyor.

--- CollabCardsTests/BoardViewModel/BoardViewModelTests.swift
+++ CollabCardsTests/BoardViewModel/BoardViewModelTests.swift
@@ -10,52 +10,24 @@ import XCTest
 
 class BoardViewModelTests: XCTestCase {
 
-    var boardViewModel: BoardViewModel!
-
-    override func setUpWithError() throws {
-        boardViewModel = BoardViewModel()
-    }
-
-    override func tearDownWithError() throws {
-        boardViewModel = nil
-    }
-
     func testFetchBoards() {
-        // Act
-        boardViewModel.fetchBoards()
-        
-        // Assert
-        XCTAssertEqual(boardViewModel.boards.count, 0, "Başlangıçta board listesi boş olmalı.")
-    }
-    
-    func testDeleteBoardLocally() {
-        // Arrange
-        let board = Board(id: UUID(), name: "Test Board", deviceID: "4232BGSDV", participants: ["Test User"])
-        boardViewModel.boards.append(board)
-        
-        // Act
-        boardViewModel.deleteBoardLocally(board)
-        
-        // Assert
-        XCTAssertFalse(boardViewModel.boards.contains(where: { $0.id == board.id }), "Board başarılı bir şekilde silinmelidir.")
-    }
-    
-    func testDeleteBoardWithFirebase() {
-        // Arrange
-        let board = Board(id: UUID(), name: "Test Board", deviceID: "06947DSJds", participants: ["Test User"])
-        boardViewModel.addBoard(board)
-        
-        let expectation = self.expectation(description: "Board silinme işlemi Firebase'de tamamlanmalıdır.")
+        let viewModel = BoardViewModel()
 
-        // Act
-        boardViewModel.deleteBoard(board)
-
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-            // Assert
-            XCTAssertFalse(self.boardViewModel.boards.contains(where: { $0.id == board.id }), "Board başarılı bir şekilde silinmelidir.")
-            expectation.fulfill()
-        }
+        XCTAssertTrue(viewModel.boards.isEmpty, "Başlangıçta tahtalar listesi boş olmalı.")
         
-        wait(for: [expectation], timeout: 5.0)
+        viewModel.boards.append(Board(id: UUID(), name: "Test Tahtası", deviceID: "DSFDSFDSFDS", participants: ["DDDDDD"]))
+
+        XCTAssertEqual(viewModel.boards.count, 1, "Bir tahta eklendiğinde, tahtalar listesi bir eleman içermeli.")
+    }
+
+    func testUpdateBoardSettings() {
+        let viewModel = BoardViewModel()
+        let boardID = UUID()
+
+        viewModel.updateBoardSettings(boardID: boardID, isDateVisible: true, isMoveCardsDisabled: true, isAddEditCardsDisabled: true, hideCards: true)
+
+        XCTAssertTrue(true, "Ayarlar güncellemesi başarılı bir şekilde tamamlanmalı.")
     }
 }
+
+
